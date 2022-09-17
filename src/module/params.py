0eba0ff9d6bec913ebb57a69a6aec8abaecccd45
Jakub Grzelak <60741674+kubagrzelak@users.noreply.github.com>
2022-09-17 20:09:25 +0100

    Update params.py

--- src/module/params.py
+++ src/module/params.py
@@ -1,7 +1,7 @@
 from os import getenv
 
 PARAMS = {
-    "MQTT_BROKER": getenv("MQTT_BROKER", "mqtt://test.mosquitto.org"),
+    "MQTT_BROKER": getenv("MQTT_BROKER", "test.mosquitto.org"),
     "PORT": int(getenv("PORT", 1883)),
     "TOPIC": getenv("TOPIC", "weeve/factory"),
     "QOS": int(getenv("QOS", 0)),
